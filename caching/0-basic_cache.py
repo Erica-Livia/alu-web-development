d22d6e963316890ad277bf06dd8c21b337c38239
Erica-Livia <e.ingabire1@alustudent.com>
2024-05-19 09:32:53 +0200

    0-basic

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,36 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""Basic cache"""
 
-
-class BaseCaching:
-    """"
-    BaseCaching
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initialize the class """
-        self.cache_data = {}
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ BasicCache """
+    """Basic cache"""
 
-    def __init_(self):
-        """
-        Initialize the class using the parent class initializer
-        """
-        super().__init()
     def put(self, key, item):
-        """
-        Add an item in the cache.
-        """
-        if key is not None and item is not None:
-            self.cache_data[key] = item
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
-        """
-        Get an item by Key
-        """
-        if key is None:
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
             return None
-        return self.cache_data.get(key)
+        return self.cache_data[key]
